3a44103254489f83128af579cd12a94e9fff9076
Phil E. Taylor <phil@phil-taylor.com>
2018-09-13 22:29:59 +0100

    dist

---
+++ dist/resetJoomla.sh
@@ -0,0 +1,3 @@
+docker-compose rm -f -s joomla250 joomla300 joomla310 joomla320 joomla330 joomla340 joomla350 joomla360 joomla370 joomla380 joomla390
+docker-compose build    joomla250 joomla300 joomla310 joomla320 joomla330 joomla340 joomla350 joomla360 joomla370 joomla380 joomla390
+docker-compose up -d    joomla250 joomla300 joomla310 joomla320 joomla330 joomla340 joomla350 joomla360 joomla370 joomla380 joomla390
